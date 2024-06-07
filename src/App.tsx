c205e2a3947e908c8bf23593317d1811c1a3ac34
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2024-06-07 14:30:13 +0530

    async actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import { selectCount } from "./state/counter/counterSelector";
-import { increment, reset, decrement, incrementByAmount } from "./state/counter/counterSlice";
+import { increment, reset, decrement, incrementByAmount, incrementAsync } from "./state/counter/counterSlice";
+import { AppDispatch } from "./state/store";
 
 function App() {
 	const count = useSelector(selectCount);
-	const dispatch = useDispatch();
+	const dispatch = useDispatch<AppDispatch>();
 
 	return (
 		<>
@@ -39,6 +40,13 @@ function App() {
 					}}
 				>
 					Increment by Amount
+				</button>
+				<button
+					onClick={() => {
+						dispatch(incrementAsync(10));
+					}}
+				>
+					Increment Asynchronous
 				</button>
 			</div>
 		</>
--- src/state/counter/counterSlice.ts
+++ src/state/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 interface CounterState {
     count: number
@@ -25,7 +25,26 @@ export const CounterSlice = createSlice({
             state.count += action.payload
         }
     },
+    extraReducers(builder) {
+        builder.addCase(incrementAsync.pending, () => {
+            console.log("incrementAsync pending");
+            
+        }).addCase(incrementAsync.fulfilled, (state, action: PayloadAction<number>) => {
+            state.count += action.payload
+        })
+    },
 })
+
+export const incrementAsync = createAsyncThunk(
+    "counter/incrementAsync",
+    async (amount: number) => {
+        await new Promise((resolve) => {
+            setTimeout(resolve, 2000)
+        })
+
+        return amount;
+    }
+)
 
 export const { increment, reset, decrement, incrementByAmount } = CounterSlice.actions
 
